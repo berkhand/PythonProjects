63b0adf40d645309d5a6a06a397dd8a89ff1f4e8
Berkhan Deniz <berkhand@gmail.com>
2023-04-10 23:16:28 +0200

    final version of quizzler app

--- quizzler-app/quiz_brain.py
+++ quizzler-app/quiz_brain.py
@@ -11,6 +11,9 @@ class QuizBrain:
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
 
+    def is_last_question(self) -> bool:
+        return self.question_number == len(self.question_list)
+
     def next_question(self):
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
@@ -18,13 +21,15 @@ class QuizBrain:
         # user_answer = input(f"Q.{self.question_number}: {html.unescape(self.current_question.text)} (True/False): ")
         # self.check_answer(user_answer)
 
-    def check_answer(self, user_answer):
+    def check_answer(self, user_answer: str) -> bool:
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
-            print("You got it right!")
+            print("check ans true")
+            return True
         else:
-            print("That's wrong.")
+            print("check ans false")
+            return False
 
-        print(f"Your current score is: {self.score}/{self.question_number}")
-        print("\n")
+    def get_score(self) -> int:
+        return self.score
--- quizzler-app/ui.py
+++ quizzler-app/ui.py
@@ -14,70 +14,58 @@ class QuizInterface:
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
 
         self.canvas = Canvas(width=300, height=350, highlightthickness=0, bg=THEME_COLOR)
-        self.score_text = self.canvas.create_text(250, 10, text="Score: 0")
-        self.canvas.create_rectangle(0, 50, 300, 300, fill="white")
-        self.question_text = self.canvas.create_text(150, 150, text="1234567890123456789",
+        self.score_text = self.canvas.create_text(250, 10, text="Score: 0", fill="white")
+        self.rect = self.canvas.create_rectangle(0, 50, 300, 300, fill="white")
+        self.question_text = self.canvas.create_text(150, 150, text="question",
                                                      font=("Ariel", 20, "italic"), fill=THEME_COLOR, width=280)
         self.canvas.grid(column=0, row=0, columnspan=2)
 
         img_true = PhotoImage(file="images/true.png")
-        button_true = Button(image=img_true, highlightthickness=0, borderwidth=0)
+        button_true = Button(image=img_true, highlightthickness=0, borderwidth=0, command=self.check_reply_true)
         button_true.grid(column=0, row=1)
 
         img_false = PhotoImage(file="images/false.png")
-        button_false = Button(image=img_false, highlightthickness=0, borderwidth=0)
+        button_false = Button(image=img_false, highlightthickness=0, borderwidth=0, command=self.check_reply_false)
         button_false.grid(column=1, row=1)
 
         self.update_question()
+        self.is_last_question_graded = False
 
         self.window.mainloop()
 
     def update_question(self):
         self.canvas.itemconfig(self.question_text, text=self.quiz.next_question())
 
+    def check_reply_logic(self, reply: str):
+        if self.quiz.still_has_questions() or (self.quiz.is_last_question() and not self.is_last_question_graded):
+            if self.quiz.check_answer(reply):
+                self.update_bg_green()
+            else:
+                self.update_bg_red()
+            self.update_score(self.quiz.get_score())
+        # if self.quiz.still_has_questions():
+        #     self.update_question()
 
-# window = Tk()
-# window.title("Flashy")
-# window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-#
-# canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
-# card_front = PhotoImage(file="images/card_front.png")
-# card_back = PhotoImage(file="images/card_back.png")
-# img = canvas.create_image(400, 263, image=card_front)
-# text1 = canvas.create_text(400, 150, text="text1", font=("Ariel", 40, "italic"))
-# text2 = canvas.create_text(400, 263, text="text2", font=("Ariel", 60, "bold"), fill="black")
-# canvas.grid(column=0, row=0, columnspan=2)
-#
-# img_right = PhotoImage(file="images/right.png")
-# button_right = Button(image=img_right, highlightthickness=0, command=approve_pressed)
-# button_right.grid(column=1, row=1)
-#
-# img_wrong = PhotoImage(file="images/wrong.png")
-# button_wrong = Button(image=img_wrong, highlightthickness=0, command=decline_pressed)
-# button_wrong.grid(column=0, row=1)
+    def check_reply_true(self):
+        self.check_reply_logic("True")
 
+    def check_reply_false(self):
+        self.check_reply_logic("False")
 
-# def get_random_word():
-#     global selection
-#     selection = random.choice(translations) #list(translations.items())
-#     canvas.itemconfig(img, image=card_front)
-#     canvas.itemconfig(text1, text="Dutch", fill="black")
-#     canvas.itemconfig(text2, text=selection["Dutch"], fill="black")
-#     start_timer()
-#
-#
-# def approve_pressed():
-#     translations.remove(selection)
-#     data = pandas.DataFrame(translations)
-#     data.to_csv("data/dutch_words_to_learn.csv", index=False)
-#     get_random_word()
-#
-#
-# def decline_pressed():
-#     get_random_word()
-#
-#
-# def flip_card():
-#     canvas.itemconfig(img, image=card_back)
-#     canvas.itemconfig(text1, text="English", fill="white")
-#     canvas.itemconfig(text2, text=selection["English"], fill="white")
+    def update_score(self, new_score):
+        self.canvas.itemconfig(self.score_text, text=f"Score: {new_score}")
+        if self.quiz.is_last_question():
+            self.is_last_question_graded = True
+
+    def update_bg_green(self):
+        self.canvas.itemconfig(self.rect, fill="green")
+        self.window.after(1000, self.update_bg_white)
+
+    def update_bg_red(self):
+        self.canvas.itemconfig(self.rect, fill="red")
+        self.window.after(1000, self.update_bg_white)
+
+    def update_bg_white(self):
+        self.canvas.itemconfig(self.rect, fill="white")
+        if self.quiz.still_has_questions():
+            self.update_question()
