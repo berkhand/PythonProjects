4c992ec96cf8b359826684f347720f0341c77016
Berkhan Deniz <berkhand@gmail.com>
2023-04-17 21:45:00 +0200

    Rain alert first version

--- rain_alert/main.py
+++ rain_alert/main.py
@@ -1,18 +1,30 @@
 import requests
 
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 API_KEY = "af829f94eae434efba38efc3f64685c2"
+MY_LAT = 51.3180109
+MY_LONG = 8.3062763
 
 
-def get_quote():
-    try:
-        response = requests.get(url="https://api.kanye.rest/")
-        response.raise_for_status()
-        quote = response.json()["quote"]
-        canvas.itemconfig(quote_text, text=quote)
-    except:
-        pass
+parameters = {
+    "lat": MY_LAT,
+    "lon": MY_LONG,
+    "appid": API_KEY,
+}
 
+response = requests.get(url=OWM_Endpoint, params=parameters)
 
+response.raise_for_status()
 
+data = response.json()
 
-get_quote()
+new_list = []
+
+for item in data["list"]:
+    new_list.append(item["weather"][0]["id"])
+
+if new_list[0] < 700 or new_list[1] < 700:
+    print("Raining")
+else:
+    print("Not raining")
+
